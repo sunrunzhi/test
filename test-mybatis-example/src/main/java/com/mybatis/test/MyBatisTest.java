554bf5564310487a49d046b46dfb35f48689561e
yc <yc@YC-PC>
2016-11-17 19:20:05 +0800

    Mybatis测试项目

---
+++ test-mybatis-example/src/main/java/com/mybatis/test/MyBatisTest.java
@@ -0,0 +1,35 @@
+package com.mybatis.test;
+
+import java.io.InputStream;
+
+import com.mybatis.pojo.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+
+public class MyBatisTest {
+
+	public static void main(String[] args) {
+		//mybatis的配置文件
+        String resource = "config.xml";
+        //使用类加载器加载mybatis的配置文件（它也加载关联的映射文件）
+        InputStream is = MyBatisTest.class.getClassLoader().getResourceAsStream(resource);
+        //构建sqlSession的工厂S
+        
+        SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(is);
+        //使用MyBatis提供的Resources类加载mybatis的配置文件（它也加载关联的映射文件）
+        SqlSession session = sessionFactory.openSession();
+        /**
+         * 映射sql的标识字符串
+         * com.mybatis.mapper.userMapper是userMapper.xml文件中mapper标签的namespace属性的值，
+         * getUser是select标签的id属性值，通过select标签的id属性值就可以找到要执行的SQL
+         */
+        String statement = "com.mybatis.mapper.userMapper.getUser";//映射sql的标识字符串
+        //执行查询返回一个唯一user对象的sql
+        User user = session.selectOne(statement, 1);
+        System.out.println(user);
+	}
+
+}
